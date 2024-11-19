07645505e2f98a9dca9306097d4455b4b13da367
kjt-123 <keirajaynetaylor@gmail.com>
2024-11-19 15:32:19 +0100

    Update streamlit_orders.py
    Testing title

--- streamlit_orders.py
+++ streamlit_orders.py
@@ -1,10 +1,14 @@
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
+import pandas as pd
 
 # Create a connection object.
 conn = st.connection("gsheets", type=GSheetsConnection)
 
 df = conn.read(worksheet="menu_items")
+
+#title of app
+st.title(":taco: :green salad: Welcome to Jacapaca! :clinking beer mugs:")
 
 # Display the data.
 #for row in df.itertuples():
