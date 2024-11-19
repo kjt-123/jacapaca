5426b430cc9590e5030b7c7e9939e93dab62b2da
kjt-123 <keirajaynetaylor@gmail.com>
2024-11-19 21:29:07 +0100

    Update streamlit_orders.py

--- streamlit_orders.py
+++ streamlit_orders.py
@@ -5,13 +5,16 @@ import pandas as pd
 # Create a connection object.
 conn = st.connection("gsheets", type=GSheetsConnection)
 
-df = conn.read(worksheet="menu_items")
+menu_df = conn.read(worksheet="menu_items").to_pandas()
+party_df = conn.read(worksheet="orders", usecols=[0,1]).to_pandas()
 
 #title of app
 st.title(":taco: Bienvenido a Jacapaca! :beers:")
 username = st.text_input("Write your name:")
 party_id = st.text_input("Hi " + username + "! Write your party id:")
-check_party = st.button('Check party details')
+party = party_df[party_df['party_id'] == party_id]
+st.write('Hi ', username, '. Tonight you are dining with:', party, '. If this is correct hit submit, otherwise please talk to your friends and choose a new party id!')
+join_party = st.button('Click to submit')
 
 # Display the data.
 #for row in df.itertuples():
